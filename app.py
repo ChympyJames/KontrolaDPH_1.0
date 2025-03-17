dd441a62a3d46730303c66aac347227c656a1048
JJKuzica <143068027+ChympyJames@users.noreply.github.com>
2025-03-17 21:59:31 +1000

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,6 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
-
 
 # --- Cache Selenium WebDriver ---
 @st.cache_resource
